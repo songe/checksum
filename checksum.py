83bdbf5379ea2f422003bcba868e8950942c0877
Angie Song <songe@berkeley.edu>
2015-04-19 18:22:47 -0700

    Update to handle urls strings with multiple query vars

--- checksum.py
+++ checksum.py
@@ -1,37 +1,75 @@
 from flask import Flask, abort, request
+from urlparse import urlparse, parse_qsl
 import hashlib
 
 app = Flask(__name__)
 
+ALLOW_NESTED_CHECKSUM = True
+
 @app.route('/create-checksum')
 def create_checksum():
-    url = get_query_param('url')
+    url = get_url(request, ALLOW_NESTED_CHECKSUM)
+
     return "{url}&checksum={checksum}".format(url=url, checksum=get_sha1(url))
 
 @app.route('/check-checksum')
 def check_checksum():
-    url = get_query_param('url')
-    checksum = get_query_param('checksum')
+    url = get_url(request)
+    checksum = get_checksum(request)
 
     if verify_checksum(url, checksum):
         return 'verified'
     else:
         abort(400)
 
-def get_query_param(name, abortIfNone=True):
-    value = request.args.get(name)
-    app.logger.debug('%s: %s', name, value)
+def get_url(request, includeChecksum=False):
+    params = parse_qsl(request.query_string)
+    url = ''
+    append = False
+    for param in params[:-1]:
+        if param[0] == 'url':
+            url = param[1]
+            if contains_query_string(url):
+                append = True # include the rest of the querystring as part of the URL
+            else:
+                break
+        elif append:
+            url += to_query_string(param)
 
-    if abortIfNone and value is None:
+    # handle last/only parameter
+    if params:
+        param = params[-1]
+        if (param[0] == 'url'):
+            url = param[1]
+        elif (append and (param[0] != 'checksum' or includeChecksum)):
+            url += to_query_string(param)
+
+    if not url:
         abort(400)
 
-    return value
+    return url
+
+def to_query_string(param):
+    return '&{name}={value}'.format(name=param[0], value=param[1])
+
+def get_checksum(request):
+    values = request.args.getlist('checksum')
+
+    if not values:
+        abort(400)
+
+    return values[-1]
+
+def contains_query_string(url):
+    parsed = urlparse(url)
+    return not not parsed.query
 
 def get_sha1(string):
     return hashlib.sha1(string).hexdigest()
 
 def verify_checksum(string, expected):
     actual = get_sha1(string)
+    app.logger.debug('checksum: expected={0}, actual={1}'.format(expected, actual))
     return actual == expected
 
 if __name__ == '__main__':
--- checksum_test.py
+++ checksum_test.py
@@ -14,13 +14,28 @@ class ChecksumTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, 'verified')
 
+    def test_check_checksum_with_checksum_in_front(self):
+        response = self.tester.get('/check-checksum?checksum=738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1&url=http://www.google.com')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'verified')
+
     def test_create_checksum_with_nested_querystring(self):
         response = self.tester.get('/create-checksum?url=http://www.google.com?q=foo&fb=x&g=y')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, 'http://www.google.com?q=foo&fb=x&g=y&checksum=738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1')
+        self.assertEqual(response.data, 'http://www.google.com?q=foo&fb=x&g=y&checksum=59f9d86879dfb1020418149d30a7285e0233ba8b')
 
     def test_check_checksum_with_nested_querystring(self):
-        response = self.tester.get('/check-checksum?url=http://www.google.com?q=foo&fb=x&g=y')
+        response = self.tester.get('/check-checksum?url=http://www.google.com?q=foo&fb=x&g=y&checksum=59f9d86879dfb1020418149d30a7285e0233ba8b')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'verified')
+
+    def test_create_checksum_with_nested_checksum(self):
+        response = self.tester.get('/create-checksum?url=http://www.google.com?q=foo&fb=x&g=y&checksum=59f9d86879dfb1020418149d30a7285e0233ba8b')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'http://www.google.com?q=foo&fb=x&g=y&checksum=59f9d86879dfb1020418149d30a7285e0233ba8b&checksum=1955556fcbdf0687fdc96a2e45119c7c218064b5')
+
+    def test_check_checksum_with_nested_checksum(self):
+        response = self.tester.get('/check-checksum?url=http://www.google.com?q=foo&fb=x&g=y&checksum=59f9d86879dfb1020418149d30a7285e0233ba8b&checksum=1955556fcbdf0687fdc96a2e45119c7c218064b5')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, 'verified')
 
@@ -30,6 +45,10 @@ class ChecksumTestCase(unittest.TestCase):
 
     def test_check_checksum_with_bad_checksum(self):
         response = self.tester.get('/check-checksum?url=http://www.google.com&checksum=BAD_CHECKSUM')
+        self.assertEqual(response.status_code, 400)
+
+    def test_check_checksum_with_no_checksum(self):
+        response = self.tester.get('/check-checksum?url=http://www.google.com')
         self.assertEqual(response.status_code, 400)
 
     def setUp(self):
